6dea62d5b0d62533304fc71757423ae507e97d80
maiquel <maiquel_craash@hotmail.com>
2016-08-18 21:50:50 -0300

    Efetuada integracão do campo de busca com a API do WIKI e criada animacão para o topo, elém de processar resultados simultaneamente.

--- main.js
+++ main.js
@@ -6,6 +6,7 @@
 	"use strict";
 	$(document).ready(function () {
 		$('.search-wrapper button, .search-wrapper span').click(searchToggle);
+		// $('.search-icon')
 	});
 
 
@@ -16,6 +17,8 @@
 			container.addClass('active');
 			evt.preventDefault();
 		}
+
+		//valida se já está aberto já o campo (active) e se o elemento clicado é o "X"
 		else if (container.hasClass('active') && $(this).closest('.input-holder').length == 0) {
 			container.removeClass('active');
 			// clear input
@@ -24,6 +27,50 @@
 			container.find('.result-container').fadeOut(100, function () {
 				$(this).empty();
 			});
+
+			if (container.hasClass('top')) {
+				container.removeClass('top', 700, "easeInOutQuart");
+			}
+		}
+		else if (container.hasClass('active') && $(this).closest('.input-holder').length > 0) {
+			var inputField = container.find('.search-input');
+			var searchString = inputField.val().trim().toLowerCase();
+
+			if (searchString) {
+
+				doSearch(searchString);
+
+				// container.animate({top: "10%"},300, function () {});
+				container.addClass('top', 700, "easeInOutQuart");
+			}
 		}
 	}
+
+	function doSearch(value) {
+		$.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&limit=5&namespace=0&format=json&search=" + value, RenderResults)
+	}
+
+	function RenderResults(data) {
+		var container = $('.container');
+
+		onAnimationFinished( function () {
+			data[1].forEach( function (element) {
+				$(container).append('<div>' + element + '</div>');
+				console.log(element);
+			});
+		})
+	}
+
+	function onAnimationFinished(callback) {
+		// Test if ANY/ALL page animations are currently active
+
+		var testAnimationInterval = setInterval(function () {
+			if (!$.timers.length) { // any page animations finished
+				clearInterval(testAnimationInterval);
+				callback();
+			}
+		}, 25);
+	}
+
 }());
+
